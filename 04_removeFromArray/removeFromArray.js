8854f14d9be426744fbe30e043730fc0738b1ee4
Farris Swisher <farrisswisher@tutanota.com>
2022-06-20 15:28:22 -0500

    Create function to remove values from array, un-skip tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
+    let args = [...arguments];
 
+    for (let i = 0; i<args[0].length; i++){
+        for (let k = 1; k<args.length; k++){
+            if (args[0][i] === args[k]){
+            args[0].splice(i, 1);
+            i--;
+            }
+        }
+    }
+
+    return args[0];
 };
 
 // Do not edit below this line
