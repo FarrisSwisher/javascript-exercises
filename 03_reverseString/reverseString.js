5ba25b4d174e70998a9edc9a2bb0f2620b11a711
Farris Swisher <farrisswisher@tutanota.com>
2022-06-20 14:21:40 -0500

    Write reverseString function, Pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const chars = [];
+    let revString = '';
 
+    for (let i = 0; i<string.length; i++){
+        chars[i] = string.charAt(i);
+    }
+
+    for (let i = 0; i<string.length; i++){
+        revString += chars[chars.length-1-i];
+    }
+
+    return revString;
 };
 
 // Do not edit below this line
