61017414a5515d8cb181627d7734f981345203fb
Farris Swisher <farrisswisher@tutanota.com>
2022-06-20 16:04:05 -0500

    Create methods for converting between Fahrenheit and Celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(degF) {
+  const degC = (degF - 32) * (5/9);
+  return +degC.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(degC) {
+  const degF = degC * (9/5) + 32;
+  return +degF.toFixed(1);
 };
 
 // Do not edit below this line
