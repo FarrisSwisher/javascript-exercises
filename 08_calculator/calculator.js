445997f2d4e24da88899a361331feeed8c9fe665
Farris Swisher <farrisswisher@tutanota.com>
2022-06-24 09:37:56 -0500

    Create calculator methods, un-skip tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	if (numArray.length === 0) return 0;
+  let temp = 0;
+  for (const num of numArray){
+    temp += num;
+  }
+  return temp;
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  if (numArray.length === 0) return 0;
+  let temp = 1;
+  for (const num of numArray){
+    temp *= num;
+  }
+  return temp;
 };
 
-const power = function() {
-	
+const power = function(num, pwr) {
+	return num ** pwr;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  else if (num === 1) return 1;
+  else if (num === 2) return 2;
+  let temp = 1;
+  for (let i = 0; i<num; i++){
+    temp *= num - i;
+  }
+  return temp;
 };
 
 // Do not edit below this line
