730793e1364aba3e6e49822865df6eeee6afc1ef
Farris Swisher <farrisswisher@tutanota.com>
2022-06-24 09:57:51 -0500

    Create method to tell if string is a Palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const regex = /[ !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    string = string.replace(regex, '');
+    string = string.toLowerCase();
+    if (string === string.split("").reverse().join("")) return true;
+    else return false;
 };
 
 // Do not edit below this line
