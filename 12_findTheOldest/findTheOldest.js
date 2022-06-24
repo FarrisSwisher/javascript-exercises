0fa4b52633f95acfd3799493b1a25486fe21f2d4
Farris Swisher <farrisswisher@tutanota.com>
2022-06-24 13:52:28 -0500

    Find the oldest person in a list of person objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson = {};
+    const d = new Date();
+    for (const person of people){
+        if (!("yearOfDeath" in person)) person["yearOfDeath"] = d.getFullYear();
+        if (oldestAge < (person.yearOfDeath - person.yearOfBirth)){
+            oldestAge = person.yearOfDeath - person.yearOfBirth;
+            oldestPerson = person;
+        }
+    }
+
+    return oldestPerson;
 
 };
 
